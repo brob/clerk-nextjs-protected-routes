998afdfa42a783ba16b921b5accee9be0efa4c86
brob <bryanlrobinson@gmail.com>
2025-02-05 15:25:51 -0500

    Wraps sign in button

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -5,7 +5,7 @@ export default function Fallback() {
         <div className="flex flex-col items-center justify-center py-5 bg-gray-100">
             <h1 className="text-3xl font-bold mb-4">This is a paid article</h1>
             <p className="text-lg mb-6">You need to be signed in to view this page</p>
-            <SignInButton mode="modal" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" />
+            <SignInButton mode="modal"><button  className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Sign In</button></SignInButton>
         </div>
     )
 }

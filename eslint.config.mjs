72a8a8142af6f82ee21c3419ae101eb4542eb131
brob <bryanlrobinson@gmail.com>
2025-02-05 15:06:07 -0500

    Removes a couple eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      "no-explicit-any": "off",
+      "no-unused-vars": "off",
+    }
+  })
 ];
 
 export default eslintConfig;
